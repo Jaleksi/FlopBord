f8c01623d244f36e411b17d33404db50615d3257
Aleksi <aleksi.jok@gmail.com>
2019-06-14 17:22:22 +0300

    collision.py

--- flopbord/collision.py
+++ flopbord/collision.py
@@ -3,10 +3,13 @@ def withinBounds(x, y, radius):
 
 
 def hittingWall(birdCord, wallCord):
-    if(wallCord[0]+wallCord[2] > birdCord[0] > wallCord[0] and
-       wallCord[1]+wallCord[3] > birdCord[1] > wallCord[1]):
+    if(wallCord[0] < birdCord[0] < wallCord[0]+wallCord[2] and
+       wallCord[1] < birdCord[1] < wallCord[1]+wallCord[3]):
         return True
     return False
+
+# birdCord[0] = x, birdCord[1] = y
+# wallCord[0] = x, wallCord[1] = y, ...[2] = leveys, ... [3] = korkeus
 
 
 if(__name__ == "__main__"):

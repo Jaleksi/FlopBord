859a54995ba97c231f2bfc9f8cce6f3243aa0564
Aleksi <aleksi.jok@gmail.com>
2019-06-14 16:10:28 +0300

    collision.py

--- flopbord/collision.py
+++ flopbord/collision.py
@@ -1,5 +1,12 @@
-def outOfBounds(x, y, radius):
+def withinBounds(x, y, radius):
     return 0+radius < x < 600-radius and 0+radius < y < 600-radius
+
+
+def hittingWall(birdCord, wallCord):
+    if(wallCord[0]+wallCord[2] > birdCord[0] > wallCord[0] and
+       wallCord[1]+wallCord[3] > birdCord[1] > wallCord[1]):
+        return True
+    return False
 
 
 if(__name__ == "__main__"):
